dfa8d6b5b11e6346e2fcadbe1cdb3a766a2d6c15
RatchagreeA <ratchagree@gmail.com>
2022-03-16 23:00:08 +0700

    delete comment in Display.java

--- Java/Display.java
+++ Java/Display.java
@@ -1,10 +1,6 @@
 class Display {
     public static void main(String[] args) {
         // Display text
-        /*
-        text1
-        text2
-        */
         System.out.print("Hi user");
         System.out.println("We are learning");
     }
