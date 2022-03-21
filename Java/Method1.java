67184c7f47b282a0631eb173af9bd5061be6118e
RatchagreeA <ratchagree@gmail.com>
2022-03-21 23:16:16 +0700

    Practice method example

--- Java/Method1.java
+++ Java/Method1.java
@@ -4,8 +4,25 @@ public class Method1 {
         plus(10,2);
         String txt =  getText();
         display(txt);
-        int[] arrT = {0, 1, 2, 3, 4, 5};
+        int[] arrT = {1, 5, 77, 44, 99, 65};
         dispArray(arrT);
+
+        int result;
+        result = findMax(arrT);
+        System.out.println("Max = " + result);
+        result = findMin(arrT);
+        System.out.println("Min = " + result);
+
+        result = findMaxIdx(arrT);
+        System.out.println("MaxIdx = " + result);
+        result = findMinIdx(arrT);
+        System.out.println("MinIdx = " + result);
+
+
+
+
+
+
     }
     private static void display(String text){
         System.out.println(text);
@@ -22,5 +39,41 @@ public class Method1 {
             String x = String.valueOf("-------");
             display(x);
         }
+    }
+    private static int findMax(int[] arr){
+        int maxVal = arr[0];
+        for (int i = 0;i<arr.length;i++){
+            if (arr[i]>maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+    private static int findMin(int[] arr){
+        int minVal = arr[0];
+        for (int i = 0;i<arr.length;i++){
+            if (arr[i]<minVal) {
+                minVal = arr[i];
+            }
+        }
+        return minVal;
+    }
+    private static int findMaxIdx(int[] arr){
+        int maxIdx = 0;
+        for (int i = 0;i<arr.length;i++){
+            if (arr[i]>arr[maxIdx]) {
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+    private static int findMinIdx(int[] arr){
+        int minIdx = 0;
+        for (int i = 0;i<arr.length;i++){
+            if (arr[i]<arr[minIdx]) {
+                minIdx = i;
+            }
+        }
+        return minIdx;
     }
 }
