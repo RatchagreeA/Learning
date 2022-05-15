52c8e3a34ce4237f1068c9b2d20f9e35f0aeab01
RatchagreeA <ratchagree@gmail.com>
2022-05-15 22:55:55 +0700

    practice Intermediate Algorithm Scripting

--- JavaScript/Itm_Algo.js
+++ JavaScript/Itm_Algo.js
@@ -1,3 +1,203 @@
+function checkCashRegister(price, cash, cid) {
+  const pair = {
+    "PENNY": 0.01,
+    "NICKEL": 0.05,
+    "DIME": 0.1,
+    "QUARTER": 0.25,
+    "ONE": 1,
+    "FIVE": 5,
+    "TEN": 10,
+    "TWENTY": 20,
+    "ONE HUNDRED": 100
+  }
+  let unit = Object.keys(pair).map( (val,idx) => [val, Object.values(pair)[idx]] );
+  unit.sort( (a,b) => b[1]-a[1]);
+  console.log(unit);
+  let change=cash-price;
+  let nCid = cid.map( arr => [arr[0], Math.round(arr[1]/pair[arr[0]]) ])
+  cid.sort( (a,b) => b[1]-a[1]);
+  return nCid;
+}
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+
+
+
+
+function telephoneCheck(str) {
+  let patt= /^(1\s?)?(\d{3}\-\d{3}\-\d{4}|\(\d{3}\)\s?\d{3}\-\d{4}|\d{3}\s?\d{3}\s?\d{4})$/;
+  return patt.test(str);
+}
+console.log(telephoneCheck("555-555-5555"));
+
+function rot13(str) {
+  const cnvChar = function( c ) {
+    let x = c.charCodeAt(0);
+    let y = ((x-65+13)%26)+65;
+    let z = String.fromCharCode(y);
+    return (x>=65 && x<=91)?z:c;
+  } 
+  return str.split('').map(cnvChar).join('');
+}
+
+console.log(rot13("SERR PBQR PNZC"));
+console.log('A'.charCodeAt(0));
+console.log('Z'.charCodeAt(0));
+
+function convertToRoman(num) {
+  const cnvTho = (val)=>  {
+    let txt = '';
+    while (val>0) {
+      txt+='M';
+      val-=1;
+    }
+    return txt
+  }
+  const cnvHun = (val)=>  {
+    let txt = '';
+    while (val>0) {
+      if (val>=9) {
+          txt+='CM';
+          val-=9;
+      } else if (val>=5) {
+        txt+='D';
+        val-=5;
+      }else if (val>=4) {
+        txt+='CD';
+        val-=4;
+      } else {
+        txt+='C';
+        val-=1;
+      }
+    }
+    return txt
+  }
+  const cnvTen = (val)=>  {
+    let txt = '';
+    while (val>0) {
+      if (val>=9) {
+          txt+='XC';
+          val-=9;
+      } else if (val>=5) {
+        txt+='L';
+        val-=5;
+      }else if (val>=4) {
+        txt+='XL';
+        val-=4;
+      } else {
+        txt+='X';
+        val-=1;
+      }
+    }
+    return txt
+  }
+  const cnvUni = (val)=>  {
+    let txt = '';
+    while (val>0) {
+      if (val>=9) {
+          txt+='IX';
+          val-=9;
+      } else if (val>=5) {
+        txt+='V';
+        val-=5;
+      }else if (val>=4) {
+        txt+='IV';
+        val-=4;
+      } else {
+        txt+='I';
+        val-=1;
+      }
+    }
+    return txt
+  }
+
+  let numTho = Math.floor(num/1000);
+  let numHun = Math.floor(num%1000/100);
+  let numTen = Math.floor(num%100/10);
+  let numUni = Math.floor(num%10);
+  console.log(numTho);
+  console.log(numHun);
+  console.log(numTen);
+  console.log(numUni);
+ return cnvTho(numTho)+cnvHun(numHun)+cnvTen(numTen)+cnvUni(numUni);
+}
+console.log(convertToRoman(3999));
+
+function palindrome(str) {
+  let bf = str.replace(/[^A-Za-z0-9]/ig,'').toLowerCase();
+  let af = bf.split('').reverse().join('').toLowerCase();
+  console.log(bf);
+  console.log(af);
+  return af===bf;
+}
+console.log(palindrome("A man, a plan, a canal. Panama"));
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  return arr.map(({ name, avgAlt }) => {
+    const earth = earthRadius + avgAlt;
+    const orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earth, 3)/GM));
+    return { name, orbitalPeriod };
+  });
+}
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+
+  const orbitalPeriod = (avgAlt) => {    
+    return Math.round(2*Math.PI*Math.sqrt((earthRadius+avgAlt)**3/GM))
+  }
+  
+  return arr.reduce( (prev,cur) => prev.concat(
+    {
+      name: cur['name'],
+      orbitalPeriod: orbitalPeriod(cur['avgAlt'])
+    }
+    ),[]);
+}
+
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
+
+
+const Person = function(firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+
+  let first = firstAndLast.split(' ')[0];
+  let last = firstAndLast.split(' ')[1];
+  let full = firstAndLast;
+  
+  this.getFirstName= function() {
+    return first;
+  };
+  this.getLastName= function() {
+    return last;
+  };
+  this.getFullName= function() {
+    return full;
+  };
+  this.setFirstName= function(txt) {
+    first=txt;
+    full = [first,last].join(' ');
+  };
+  this.setLastName= function(txt) {
+    last=txt;
+    full = [first,last].join(' ');
+  };
+  this.setFullName= function(txt) {
+    full=txt;
+    first = txt.split(' ')[0];
+    last = txt.split(' ')[1];
+  };
+};
+
+const bob = new Person('Bob Ross');
+console.log(bob.setFirstName("Haskell"));
+
 function dropElements(arr, func) {
 
   // let idx = arr.map(func).indexOf(true);
@@ -24,24 +224,24 @@ function smallestCommons(arr) {
 console.log(smallestCommons([1, 5]));
 
 
-// function smallestCommons(arr) {
-//   // Setup
-//   const [min, max] = arr.sort((a, b) => a - b);
-//   const range = Array(max - min + 1)
-//     .fill(0)
-//     .map((_, i) => i + min);
-//   // GCD of two numbers
-//   // https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid's_algorithm
-//   const gcd = (a, b) => (b === 0) ? a : gcd(b, a % b);
-//   // LCM of two numbers
-//   // https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
-//   const lcm = (a, b) => a * b / gcd(a, b);
-//   // LCM of multiple numbers
-//   // https://en.wikipedia.org/wiki/Least_common_multiple#Other
-//   return range.reduce((multiple, curr) => lcm(multiple, curr));
-// }
+function smallestCommons(arr) {
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+  const range = Array(max - min + 1)
+    .fill(0)
+    .map((_, i) => i + min);
+  // GCD of two numbers
+  // https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclid's_algorithm
+  const gcd = (a, b) => (b === 0) ? a : gcd(b, a % b);
+  // LCM of two numbers
+  // https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
+  const lcm = (a, b) => a * b / gcd(a, b);
+  // LCM of multiple numbers
+  // https://en.wikipedia.org/wiki/Least_common_multiple#Other
+  return range.reduce((multiple, curr) => lcm(multiple, curr));
+}
 
-// smallestCommons([1, 5]);
+smallestCommons([1, 5]);
 
 function sumPrimes(num) {
   let sum = 0;
@@ -63,15 +263,15 @@ function isPrimes(n) {
   }
   return true
 }
-// function sumPrimes(num) {
+function sumPrimes(num) {
   
-//   let primes = [];
-//   for (let i = 2; i <= num; i++) {
-//     if (primes.every((prime) => i % prime !== 0))
-//       primes.push(i);
-//   }
-//   return primes.reduce((sum, prime) => sum + prime, 0);
-// }
+  let primes = [];
+  for (let i = 2; i <= num; i++) {
+    if (primes.every((prime) => i % prime !== 0))
+      primes.push(i);
+  }
+  return primes.reduce((sum, prime) => sum + prime, 0);
+}
 
 function convertHTML(str) {
   let pair = {
@@ -97,15 +297,15 @@ function uniteUnique(...arr) {
 console.log( uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) );
 
 
-// function uniteUnique(...arr) {
-//   return [...new Set(arr.flat())];
-// }
+function uniteUnique(...arr) {
+  return [...new Set(arr.flat())];
+}
 
-// function uniteUnique() {
-//   return [...arguments]
-//     .flat()
-//     .filter((item, ind, arr) => arr.indexOf(item) === ind);
-// }
+function uniteUnique() {
+  return [...arguments]
+    .flat()
+    .filter((item, ind, arr) => arr.indexOf(item) === ind);
+}
 
 function fearNotLetter(str) {
 
