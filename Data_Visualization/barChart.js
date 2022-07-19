648cd02ccb3f2337d3c6e9dd6a76791c13deec21
RatchagreeA <ratchagree@gmail.com>
2022-07-19 21:58:24 +0700

    add axis

--- Data_Visualization/barChart.js
+++ Data_Visualization/barChart.js
@@ -25,9 +25,53 @@ const dataUrl =
 
 async function main() {
     try {
-        const res = await fetch(url);
-        const data = await res.json();
-        console.log(data);
+        const res = await fetch(dataUrl);
+        const gdpData = await res.json();
+        console.log(gdpData);
+
+        svgContainer
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("x", -200)
+            .attr("y", 80)
+            .text("Gross Domestic Product");
+
+        var years = gdpData.data.map((item) => {
+            var temp = item[0].substring(5, 7);
+
+            if (temp === "01") {
+                return item[0].substring(0, 4) + " Q1";
+            }
+            if (temp === "04") {
+                return item[0].substring(0, 4) + " Q2";
+            }
+            if (temp === "07") {
+                return item[0].substring(0, 4) + " Q3";
+            }
+            if (temp === "10") {
+                return item[0].substring(0, 4) + " Q4";
+            }
+            return item[0].substring(0, 4);
+        });
+        console.log(years);
+        var yearsDate = gdpData.data.map((item) => {
+            return new Date(item[0]);
+        });
+        var xMax = new Date(d3.max(yearsDate));
+        xMax.setMonth(xMax.getMonth() + 3);
+        console.log(xMax);
+        var xScale = d3
+            .scaleTime()
+            .domain([d3.min(yearsDate), xMax])
+            .range([0, width]);
+
+        var xAxis = d3.axisBottom().scale(xScale);
+        console.log(xAxis);
+        svgContainer
+            .append("g")
+            .call(xAxis)
+            .attr("id", "x-axis")
+            .attr("transform", "translate(60, 400)");
     } catch (err) {
         console.log(err);
     }
